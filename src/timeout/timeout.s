8b32b5f66492d23bd6ec511e5f709bb2e70b1523
Nathan Anderson <nathandanielanderson@gmail.com>
2025-08-10 17:19:37 -0500

    Add validation and timeout logic

--- src/timeout/timeout.s
+++ src/timeout/timeout.s
@@ -3,4 +3,13 @@
 .equ CURRENT_SLOT_HEIGHT, -0x0028
 .globl entrypoint
 entrypoint:
+  ldxdw r0, [r1+NUM_ACCOUNTS]
+  ldxdw r2, [r1+MAX_SLOT_HEIGHT]
+  mov64 r1, r10
+  add64 r1, CURRENT_SLOT_HEIGHT
+  call sol_get_clock_sysvar
+  ldxdw r1, [r1+0x0000]
+  jle r1, r2, end
+  lddw r0, 1
+
 
