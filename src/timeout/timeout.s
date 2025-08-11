1d18eb557ae1dbff33147a6690d481140807bb1e
Nathan Anderson <nathandanielanderson@gmail.com>
2025-08-10 21:18:22 -0500

    trimmed cu's

--- src/timeout/timeout.s
+++ src/timeout/timeout.s
@@ -5,12 +5,8 @@
 .globl entrypoint
 entrypoint:
   ldxdw r2, [r1+MAX_SLOT_HEIGHT]
-
   ldxdw r3, [r1+CLOCK_DATA]
-  ldxdw r4, [r3+0x0000]
-  
-  lddw r0, 0
-  jle r4, r2, end
+  jle r3, r2, end
   lddw r0, 1
 end:
   exit
