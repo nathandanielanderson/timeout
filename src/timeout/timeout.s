269d5a9169dc99e47880b09eb21ab30c6484b612
Nathan Anderson <nathandanielanderson@gmail.com>
2025-08-10 21:10:55 -0500

    Corrected data structure using Blueshift tool

--- src/timeout/timeout.s
+++ src/timeout/timeout.s
@@ -1,12 +1,13 @@
 .equ NUM_ACCOUNTS, 0x0000
-.equ ACCOUNTS_PTR, 0x0008
 .equ MAX_SLOT_HEIGHT, 0x0010
+.equ CLOCK_DATA, 0x0060
 
 .globl entrypoint
 entrypoint:
   ldxdw r2, [r1+MAX_SLOT_HEIGHT]
-  ldxdw r3, [r1+ACCOUNTS_PTR]
-  ldxdw r4, [r3+0x0000 ]
+
+  ldxdw r3, [r1+CLOCK_DATA]
+  ldxdw r4, [r3+0x0000]
   
   lddw r0, 0
   jle r4, r2, end
